e7966df240af8c0769d40e8f373002b141a14876
Vicky Wen <fu3810299@gmail.com>
2020-08-01 20:36:09 +0800

    #3 UI to html, add eslint, element-ui

---
+++ resources/pages/login/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <div class="login__form">
+      <div class="login__title">SIGN IN</div>
+      <el-input
+        class="login__input"
+        placeholder="Username"
+        autofocus
+        v-model="form.account"></el-input>
+      <el-input
+        type="password"
+        class="login__input"
+        placeholder="Password"
+        v-model="form.password"></el-input>
+      <el-checkbox class="login__chkbox">Remember me</el-checkbox>
+      <el-button type="primary" class="login__btn">SIGN IN</el-button>
+      <div class="login__copyright">© 2020 CodingCat</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        account: null,
+        password: null
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" src="./style.scss"></style>
