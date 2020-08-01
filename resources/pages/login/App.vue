b98094bf08330670fb498e1a65349b7a2172beaf
Vicky Wen <fu3810299@gmail.com>
2020-08-01 23:13:10 +0800

    #3 UI to html, add utilities css, laravel layout, icon

--- resources/pages/login/App.vue
+++ resources/pages/login/App.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="login">
-    <div class="login__form">
-      <div class="login__title">SIGN IN</div>
+  <div class="login d-flex align-items-center justify-content-center position-relative">
+    <div class="login__form position-relative m-3">
+      <div class="text-center">
+        <img class="d-inline-block" alt="icon" width="70px"
+          :src="require('../../images/icon.svg')" />
+      </div>
+      <div class="h1 text-secondary text-center mb-4">SIGN IN</div>
       <el-input
-        class="login__input"
+        class="mb-3"
         placeholder="Username"
         autofocus
         v-model="form.account"></el-input>
       <el-input
         type="password"
-        class="login__input"
+        class="mb-3"
         placeholder="Password"
         v-model="form.password"></el-input>
-      <el-checkbox class="login__chkbox">Remember me</el-checkbox>
-      <el-button type="primary" class="login__btn">SIGN IN</el-button>
-      <div class="login__copyright">© 2020 CodingCat</div>
+      <el-checkbox class="text-secondary mb-4">Remember me</el-checkbox>
+      <el-button type="primary" class="mb-5 d-block w-100">SIGN IN</el-button>
+      <div class="h6 text-placeholder text-center">© {{ thisYear }} CodingCat</div>
     </div>
   </div>
 </template>
@@ -26,8 +30,12 @@ export default {
       form: {
         account: null,
         password: null
-      }
+      },
+      thisYear: null
     }
+  },
+  created () {
+    this.thisYear = new Date().getFullYear()
   }
 }
 </script>
--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,14 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Coding Cat</title>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    </head>
-    <body>
-        <div id="app"></div>
-    </body>
+@extends('layouts.admin')
+
+@section('title', ' | Admin')
+
+@section('content')
+    <div id="app"></div>
+@stop
+
+@section('footer')
     <script src="{{ asset('js/login.js') }}"></script>
-</html>
+@stop
